8c38350e707272c1904833f7d71b41baf281e79e
Asthalochan Mohanta <mohantaastha@gmail.com>
2024-03-04 04:48:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ def get_response(user_query):
 
 
 # app config
-st.set_page_config(page_title="Chat with Bhagavad Gita", initial_sidebar_state="auto", page_icon="🕉️")
+st.set_page_config(page_title="Chat with Bhagavad Gita", initial_sidebar_state="collapsed", page_icon="🕉️")
 st.title("Chat with Bhagavad Gita 🕉️")
 
 
